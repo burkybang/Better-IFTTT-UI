3ed244c47f8b85e4d889938ab50151c00a4fd251
Adam Smith <adams@annexa.com.au>
2019-09-11 16:14:35 +1000

    Improved CSS page injection

--- content.js
+++ content.js
@@ -1,24 +1,32 @@
-let style;
-
-switch (window.origin) {
-  case 'https://ifttt.com':
-    style = 'ifttt';
-    break;
-  case 'https://platform.ifttt.com':
-    style = 'ifttt_platform';
-    break;
-}
-
-if (style) {
-  const id = style + '_style';
-  const existingStyle = document.getElementById(id);
-  if (existingStyle)
-    existingStyle.parentNode.removeChild(existingStyle);
+(() => {
   
-  const link = document.createElement('link');
-  link.href = chrome.extension.getURL(chrome.runtime.getURL('css/' + style + '.css'));
-  link.id = id;
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  document.documentElement.appendChild(link);
-}
+  let style;
+  
+  switch (window.origin) {
+    case 'https://ifttt.com':
+      style = 'ifttt';
+      break;
+    case 'https://platform.ifttt.com':
+      style = 'ifttt_platform';
+      break;
+  }
+  
+  if (style) {
+    const id = style + '_style';
+    const href = chrome.extension.getURL(chrome.runtime.getURL('css/' + style + '.css') + '?_=' + Date.now());
+    
+    const existingStyle = document.getElementById(id);
+    if (existingStyle) {
+      existingStyle.href = href;
+      return;
+    }
+    
+    const link = document.createElement('link');
+    link.href = href;
+    link.id = id;
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    document.documentElement.appendChild(link);
+  }
+  
+})();
